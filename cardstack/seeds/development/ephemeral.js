d753a46c882d5d072e8f41ddd48df0b6dbeaf041
Edward Faulkner <ef@alum.mit.edu>
2017-10-05 14:24:53 -0400

    switch to mobiledoc for description fields

--- cardstack/seeds/development/ephemeral.js
+++ cardstack/seeds/development/ephemeral.js
@@ -28,7 +28,13 @@ module.exports = [
   }
 ];
 
+function asMobiledoc(message) {
+  return {"version":"0.3.1","atoms":[],"cards":[],"markups":[],"sections":[[1,"p",[[0,[],0,message]]]]}
+}
+
 function initialModels() {
+
+
   let factory = new Factory();
   factory.addResource('content-types', 'rentals')
     .withRelated('fields', [
@@ -58,7 +64,7 @@ function initialModels() {
         }),
       factory.addResource('fields', 'description')
         .withAttributes({
-          'field-type': '@cardstack/core-types::string'
+          'field-type': '@cardstack/mobiledoc'
         })
     ]);
 
@@ -91,7 +97,7 @@ function initialModels() {
     "property-type": "Estate",
     "bedrooms": 15,
     "image": "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg",
-    "description": "This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests."
+    "description": asMobiledoc("This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.")
   });
 
   factory.addResource('rentals').withAttributes({
@@ -101,7 +107,7 @@ function initialModels() {
     "property-type": "Condo",
     "bedrooms": 1,
     "image": "https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg",
-    "description": "A commuters dream. This rental is within walking distance of 2 bus stops and the Metro."
+    "description": asMobiledoc("A commuters dream. This rental is within walking distance of 2 bus stops and the Metro.")
   });
 
   factory.addResource('rentals').withAttributes({
@@ -111,7 +117,7 @@ function initialModels() {
     "property-type": "Apartment",
     "bedrooms": 3,
     "image": "https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg",
-    "description": "Convenience is at your doorstep with this charming downtown rental. Great restaurants and active night life are within a few feet."
+    "description": asMobiledoc("Convenience is at your doorstep with this charming downtown rental. Great restaurants and active night life are within a few feet.")
   });
 
   factory.addResource('pages').withAttributes({
